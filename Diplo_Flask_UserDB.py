4bfcd605b3e117662a4a0cd0054ecc509abf3ac1
Jonathan Gendu <jogendu@tsn.at>
2023-10-01 14:00:00 +0200

    JWT Token und DB Anpassung

--- Diplo_Flask_UserDB.py
+++ Diplo_Flask_UserDB.py
@@ -1,11 +1,15 @@
 import os
 import secrets
+from datetime import datetime
 
 import psycopg2
-from flask import Flask, render_template, request, url_for, redirect, flash, abort
+from flask import Flask, render_template, request, url_for, redirect, flash, abort, jsonify
+from flask_cors import CORS
 import bcrypt
+import jwt
 
 app = Flask(__name__)
+cors = CORS(app)
 
 
 def get_db_connection():
@@ -21,6 +25,15 @@ def get_db_connection():
     except psycopg2.Error as e:
         print("Error connecting to the database:", e)
         return None
+
+
+def generate_token(user_id):
+    payload = {
+        'user_id': user_id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    }
+    token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+    return token
 
 
 @app.route('/login', methods=['POST'])
@@ -44,8 +57,11 @@ def login():
         if user and bcrypt.checkpw(password.encode('utf-8'), user[2].encode('utf-8')):
             cur.close()
             conn.close()
-            users_json = {"id": user[0], "username": user[1], "password": user[2]}
-            return jsonify(users_json)
+            user_id = user[0]
+            token = generate_token(user_id)
+            user_data = {"id": user[0],  "firstname": user[1], "surname": user[2],  "username": user[3], "birthdate": user[4], "email": user[5],  "fav_sports": user[6], "gender": user[7], "postal_code": user[8]}
+            #kein Passwort
+            return jsonify({'token': token, 'user': user_data})
 
         else:
             return abort(404)
@@ -59,8 +75,16 @@ def register():
     data = request.get_json()
     if not data:
         return abort(404)
+    firstname = data.get('firstname')
+    surname = data.get('surname')
     username = data.get('username')
     password = data.get('password')
+    birthdate = data.get('birthdate')
+    email = data.get('email')
+    fav_sports = data.get('fav_sports')
+    gender = data.get('gender')
+    postal_code = data.get('postal_code')
+
     if not username or not password:
         return abort(404)
     conn = get_db_connection()
@@ -75,7 +99,9 @@ def register():
         else:
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-            cur.execute('INSERT INTO users (username, password) VALUES (%s, %s)', (username, hashed_password))
+            cur.execute(
+                'INSERT INTO users (firstname, surname, username, password, birthdate, email, fav_sports, gender, postal_code) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                (firstname, surname, username, hashed_password, birthdate, email, fav_sports, gender, postal_code))
             conn.commit()
 
             cur.close()
@@ -102,6 +128,6 @@ def get_all_users():
         return abort(404)
 
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     app.secret_key = secrets.token_hex(16)
     app.run(debug=True)
