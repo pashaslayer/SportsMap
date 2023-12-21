fb9a5a5e19823bc7ea27fd69cf767796519b55c1
pashaslayer <pavel.khakhlou@gmx.at>
2023-12-21 11:07:40 +0100

    trying to solve dependency issue

--- sport-spot/src/main.js
+++ sport-spot/src/main.js
@@ -5,9 +5,12 @@ import App from './App.vue'
 import "bootstrap/dist/js/bootstrap.js"
 import '@fortawesome/fontawesome-free/js/all'
 
+import OpenLayersMap from "vue3-openlayers";
+import "vue3-openlayers/styles.css";
 
-// importing and registration route
 import router from './router'
 
-createApp(App).use(router).mount('#app')
-
+const app = createApp(App);
+app.use(OpenLayersMap /* options */);
+app.use(router);
+app.mount("#app");
