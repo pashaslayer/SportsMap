a10f64bbd0f976760def3d4a52546375a6e3aafd
pashaslayer <pavel.khakhlou@gmx.at>
2024-02-07 21:55:26 +0100

    smtp rebuilt

--- smtp.py
+++ smtp.py
@@ -10,30 +10,32 @@ receiver_email = "pavel.khakhlou@gmx.at"
 smtp_server = "smtp-mail.outlook.com"
 smtp_port = 587  # For SSL, use 465
 smtp_user = "sportspotaustria@outlook.com"
-smtp_password = ""
+smtp_password = "4jGpP5GsQXUF9R9"
 
-# Email content
-subject = "Subject"
-body = """
-Example body
-"""
 
-# Setup MIME
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = subject
 
-# Attach the email body to the MIME message
-message.attach(MIMEText(body, "plain"))
+def send_mail(to, send_body, send_subject):
+    # Email content
+    subject = send_subject
+    body = send_body
+    # Setup MIME
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = to
+    message["Subject"] = subject
 
-# Send the email
-try:
-    # Connect to the SMTP server
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()  # Secure the connection
-        server.login(smtp_user, smtp_password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
-        print("Email sent successfully!")
-except Exception as e:
-    print(f"Failed to send email: {e}")
+    # Attach the email body to the MIME message
+    message.attach(MIMEText(body, "plain"))
+
+    # Send the email
+    try:
+        # Connect to the SMTP server
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()  # Secure the connection
+            server.login(smtp_user, smtp_password)
+            server.sendmail(sender_email, to, message.as_string())
+            print("Email sent successfully!")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+
+
